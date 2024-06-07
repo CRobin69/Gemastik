6bc86f3f6463ac7fe60c10fc4c56272cde0afa3c
Christopher Robin <williamchen1506@gmail.com>
2024-06-07 12:50:36 +0700

    `refractor : ` change abort status code

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,11 +8,11 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if ctx.Request.Method == "OPTIONS" {
-			ctx.AbortWithStatus(204)
+			ctx.AbortWithStatus(200)
 			return
 		}
 
