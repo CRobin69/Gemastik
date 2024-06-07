10b850ce6b6374823bc9ee976e7c1abed98b8eff
Christopher Robin <williamchen1506@gmail.com>
2024-06-07 22:58:51 +0700

    `refractor : ` change some configuration

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -1,18 +1,12 @@
 package middleware
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		allowedOrigin := os.Getenv("FRONTEND_DOMAIN")
-		if allowedOrigin == "" {
-			allowedOrigin = "*" 
-		}
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Length, Content-Type, Authorization, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
